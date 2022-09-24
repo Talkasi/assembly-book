9e34cc10f089d6c825f9453cc5b8d0d7223946dd
Talkasi <kitalkasi@gmail.com>
2022-09-24 11:35:02 +0300

    Remove printf

--- Final/start_man.s
+++ Final/start_man.s
@@ -1,33 +1,46 @@
-
+.include "definitions.s"
 .section .data
 
 create_line:
 	.ascii "> Type 'create' to create a new data file.\n\0"
+create_line_end:
+.equ create_line_len, create_line_end - create_line
 
 exit_line:
 	.ascii "> Type 'exit' to exit the app.\n\0"
+exit_line_end:
+.equ exit_line_len, exit_line_end - exit_line
 
 manual_line:
 	.ascii "> Type 'manual' to see the list of the hole operations supported.\n\0"
-
+manual_line_end:
+.equ manual_line_len, manual_line_end - manual_line
 
 .section .text
 
 .globl start_man
-
 .type start_man, @function
 start_man:
 	pushl %ebp
 	movl %esp, %ebp
 
-	pushl $create_line 
-	call printf
+	movl $STDOUT, %ebx
+	movl $create_line, %ecx
+	movl $create_line_len, %edx 
+	movl $SYS_WRITE, %eax
+	int $LINUX_SYSCALL
 
-	pushl $exit_line 
-	call printf
+	movl $STDOUT, %ebx
+	movl $exit_line, %ecx
+	movl $exit_line_len, %edx 
+	movl $SYS_WRITE, %eax
+	int $LINUX_SYSCALL
 
-	pushl $manual_line
-	call printf
+	movl $STDOUT, %ebx
+	movl $manual_line, %ecx
+	movl $manual_line_len, %edx 
+	movl $SYS_WRITE, %eax
+	int $LINUX_SYSCALL
 
 	movl %ebp, %esp
 	popl %ebp
